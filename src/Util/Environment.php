4e9a07abb31483f37705c91c5d5d8ed975429dc2
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2023-04-15 21:29:29 -0300

    fix util -> Util folder for psr4

---
+++ src/Util/Environment.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Util;
+
+use Dotenv\Dotenv;
+
+class Environment
+{
+    private static bool $initialized = false;
+
+    private static function initialize(): void
+    {
+        if (!self::$initialized) {
+            $dotenv = Dotenv::createImmutable(__DIR__ . '/../..');
+            $dotenv->load();
+            self::$initialized = true;
+        }
+    }
+
+    public static function getPostgresUser(): string
+    {
+        self::initialize();
+        return getenv('POSTGRES_USER');
+    }
+
+    public static function getPostgresPassword(): string
+    {
+        self::initialize();
+        return getenv('POSTGRES_PASSWORD');
+    }
+
+    public static function getPostgresDb(): string
+    {
+        self::initialize();
+        return getenv('POSTGRES_DB');
+    }
+
+    public static function getPostgresHost(): string
+    {
+        self::initialize();
+        return getenv('POSTGRES_HOST');
+    }
+
+    public static function getPostgresPort(): int
+    {
+        self::initialize();
+        return (int) getenv('POSTGRES_PORT');
+    }
+}
