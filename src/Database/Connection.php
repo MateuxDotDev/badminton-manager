1be985203b214224723cb97ba009ef98d3e6a5fd
Mateus Lucas <mateuxlucax@gmail.com>
2023-04-16 15:26:55 -0300

    feat: add deploy step on build.yml

--- src/Database/Connection.php
+++ src/Database/Connection.php
@@ -32,7 +32,7 @@ class Connection implements ConnectionInterface
                     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
                     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
                     PDO::ATTR_EMULATE_PREPARES => false,
-                ]            
+                ]
             );
 
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
