a614883e8d0fbe8a1c21f51254d4bbf73f7d4c2a
Mateus Lucas <mateuxlucax@gmail.com>
2023-04-27 22:01:39 -0300

    tests: add tests for Admin login and tournaments

---
+++ tests/Admin/Competicoes/CompeticaoTest.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace Tests\Admin\Competicoes;
+
+use App\Admin\Competicoes\Competicao;
+use DateTimeImmutable;
+use PHPUnit\Framework\TestCase;
+
+class CompeticaoTest extends TestCase
+{
+    public function testSetAndGetId()
+    {
+        $competicao = new Competicao();
+        $competicao->setId(10);
+        $this->assertEquals(10, $competicao->id());
+    }
+
+    public function testSetAndGetNome()
+    {
+        $competicao = new Competicao();
+        $competicao->setNome('Teste Nome');
+        $this->assertEquals('Teste Nome', $competicao->nome());
+    }
+
+    public function testSetAndGetPrazo()
+    {
+        $prazo = new DateTimeImmutable();
+        $competicao = new Competicao();
+        $competicao->setPrazo($prazo);
+        $this->assertEquals($prazo, $competicao->prazo());
+    }
+
+    public function testPrazoPassou()
+    {
+        $prazo = new DateTimeImmutable('yesterday');
+        $competicao = new Competicao();
+        $competicao->setPrazo($prazo);
+        $this->assertTrue($competicao->prazoPassou());
+
+        $prazo = new DateTimeImmutable('tomorrow');
+        $competicao->setPrazo($prazo);
+        $this->assertFalse($competicao->prazoPassou());
+    }
+
+    public function testToJson()
+    {
+        $prazo = new DateTimeImmutable();
+        $competicao = new Competicao();
+        $competicao->setId(10)
+            ->setNome('Teste Nome')
+            ->setPrazo($prazo);
+
+        $expected = [
+            'id' => 10,
+            'nome' => 'Teste Nome',
+            'prazo' => $prazo->format('Y-m-d')
+        ];
+
+        $this->assertEquals($expected, $competicao->toJson());
+    }
+}
