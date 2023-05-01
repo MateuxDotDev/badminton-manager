af298ccb9a7a1b61002112a93826235a9160f24b
Lucas Moraes Schwambach <lucas.schwambach52@gmail.com>
2023-05-01 18:41:03 -0300

    fix: Dates::parseXxxx methods didn't parse nulls correctly

--- src/Util/Dates.php
+++ src/Util/Dates.php
@@ -17,27 +17,27 @@ class Dates
         return DateTimeImmutable::createFromFormat($fmt, $s);
     }
 
-    private static function format(DateTimeInterface $d, string $fmt): ?string
+    private static function format(?DateTimeInterface $d, string $fmt): ?string
     {
         return $d?->format($fmt);
     }
 
-    public static function parseDay(string $s)
+    public static function parseDay(?string $s)
     {
         return self::parse($s, self::DAY);
     }
 
-    public static function parseMicro(string $s)
+    public static function parseMicro(?string $s)
     {
         return self::parse($s, self::MICRO);
     }
 
-    public static function formatDay(DateTimeInterface $d)
+    public static function formatDay(?DateTimeInterface $d)
     {
         return self::format($d, self::DAY);
     }
 
-    public static function formatMicro(DateTimeInterface $d)
+    public static function formatMicro(?DateTimeInterface $d)
     {
         return self::format($d, self::MICRO);
     }
